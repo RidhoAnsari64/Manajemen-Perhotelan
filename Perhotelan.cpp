2986aee16b6b89ea50f13367b6600c453a25ea69
RidhoAnsari64 <amak.sevenfold@gmail.com>
2024-07-13 13:22:08 +0800

    Tes Memasukkan Nama

---
+++ Perhotelan.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+int pil;
+
+void checkin()
+{
+    int nama, telepon;
+    cout << "Masukkan nama Anda = ";
+    cin >> nama;
+}
+
+int main()
+{
+    cout << "============== Selamat Datang di Hotel RR ===================" << endl;
+    cout << "Apakah Anda ingin check in atau check out?" << endl;
+    cout << "1. Check in." << endl;
+    cout << "2. Check out." << endl;
+    cout << "Masukkan pilihan Anda = ";
+    cin >> pil;
+    system ("cls");
+    switch (pil)
+    {
+        case 1:
+        checkin();
+        break;
+    }
+}
